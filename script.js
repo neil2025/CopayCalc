b7caa909e3807b2f3247af67b9079e0e07a81a8a
neil2025 <neilkush30@gmail.com>
2023-05-07 15:09:51 -0400

    bugs

--- script.js
+++ script.js
@@ -1,55 +1,59 @@
-addSection = document.getElementById('addSection');
-addSection.onsubmit = () => {
-
-}
+addSection = document.getElementById("addSection");
+addSection.onsubmit = () => {};
 
 form = document.querySelector("#Data");
 
 form.onsubmit = (e) => {
-    // console.log("test");
-    e.preventDefault();
+  // console.log("test");
+  e.preventDefault();
 
-    let data = e.target.elements;
-    main(data);
-}
-
+  let data = e.target.elements;
+  main(data);
+};
 
 function main(d) {
-    // Refactor me..... eventually
-    let yearly = parseFloat(d.max.value) - parseFloat(d.pending.value);
-    let ded = parseFloat(d.ded.value);
+  // Refactor me..... eventually
+  const max = parseFloat(d.max.value);
+  const pendingClaims = parseFloat(d.pending.value);
+  let yearly = !isNaN(max) && !isNaN(pendingClaims) ? max - pendingClaims : max;
+  let ded = parseFloat(d.ded.value);
 
-    let pay = 0;
-    for (ent of document.querySelectorAll(".procedure")) {
-        let p = parseFloat(ent.children[3].value) / 100;
-        let f = parseFloat(ent.children[1].value) - ded;
-        ded = 0;
-        pay += p * f;
-    }
+  let pay = 0;
+  for (ent of document.querySelectorAll(".procedure")) {
+    let p = parseFloat(ent.children[3].value) / 100;
+    let f = parseFloat(ent.children[1].value) - ded;
+    ded = 0;
+    pay += p * f;
+  }
 
-    if (pay > yearly) {
-        pay = yearly;
-    }
+  if (pay < 0) {
+    pay = 0;
+  }
 
-    let tf = 0;
-    for (ent of document.querySelectorAll(".procedure")) {
-        tf += parseFloat(ent.children[1].value);
-    }
-    console.log(tf, pay)
-    document.querySelector("#output").innerHTML = `Patient $${tf - pay}\nInsurance: $${pay}`; // pat = sum of fees - pay
+  if (pay > yearly) {
+    pay = yearly;
+  }
+
+  let tf = 0;
+  for (ent of document.querySelectorAll(".procedure")) {
+    tf += parseFloat(ent.children[1].value);
+  }
+  console.log(tf, pay);
+  document.querySelector("#output").innerHTML = `Patient $${
+    tf - pay
+  }\nInsurance: $${pay}`; // pat = sum of fees - pay
 }
 
 function add() {
-    let n = document.createElement("fieldset");
-    n.className = "procedure";
-    content = `
+  let n = document.createElement("fieldset");
+  n.className = "procedure";
+  content = `
             <legend>Procedure Info</legend>
             <input type="text" name="Fee" id="fee">
-            <label for="Fee">Fee</label>
+            <label for="Fee">*Fee</label>
             <input type="text" name="Percent" id="perc">
-            <label for="Percent">Percent</label>
-            <p class="close" onclick = "this.parentElement.remove()">✖</p>`
-    n.innerHTML = content
-    form.appendChild(n);
-
+            <label for="Percent">*Percent</label>
+            <p class="close" onclick = "this.parentElement.remove()">✖</p>`;
+  n.innerHTML = content;
+  form.appendChild(n);
 }
